16b9ce4324a34a3b0e6de6ba709b07bcb5983289
Alok Kumar Nayak <alokkumar.nayak2009@gmail.com>
2025-03-06 08:50:26 +0530

    '06-03-2025'

---
+++ 11.py
@@ -0,0 +1,17 @@
+# str = 'Aptech\tLearning'
+# print(str)
+
+# str1 = 'Aptech\nLearning'
+# print(str1)
+
+# str2 ='Aptech\b\bLearning'
+# print(str2)
+
+# str3 = "Aptech's Student"
+# print(str3)
+
+# str4 = 'Aptech\'s Student'
+# print(str4)
+
+str5 = 'a\\b'
+print(str5)
---
+++ 12.py
@@ -0,0 +1,4 @@
+print("RGMcollege")
+print("RGM\tcollege")
+print("RGM\ncollege")
+print('This is \' symbol')
